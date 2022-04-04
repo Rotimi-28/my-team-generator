02e3d0710f4834152b1e4b3eb5b9d9cb5a74fddb
Rotimi.E <matanmearoja@yahoo.com>
2022-04-04 16:53:42 -0700

    index.js file updates

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 const templatBuilder = require("./src/template");
 const inquirer = require('inquirer');
-const fs = require('fs')
+const fs = require('fs');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const render = require('./lib/htmlRender');
+
+// Empty arry
+let team = [];
+
+// Welcome meassage log
+const welcomeMessage = `Welcome to My Team Generator!
+please answer the series of prompts.
+Your generator file will be added to the output folder.;`
+console.log(welcomeMessage);
